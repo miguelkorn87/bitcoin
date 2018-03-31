9ab3be5e02fb51236d781a098be28f78a9f07731
Miguel Seijo <miguelkorn87@gmail.com>
2018-03-31 22:58:35 +0200

    Changed default servers to miguelkoin.duckdns.org

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,13 +129,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.miguelkoin.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
-        vSeeds.emplace_back("dnsseed.miguelkoin.dashjr.org"); // Luke Dashjr
-        vSeeds.emplace_back("seed.miguelkoinstats.com"); // Christian Decker, supports x1 - xf
-        vSeeds.emplace_back("seed.miguelkoin.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.mkn.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.miguelkoin.sprovoost.nl"); // Sjors Provoost
+        vSeeds.emplace_back("miguelkoin.duckdns.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
@@ -237,10 +231,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet-seed.miguelkoin.jonasschnelli.ch");
-        vSeeds.emplace_back("seed.tmkn.petertodd.org");
-        vSeeds.emplace_back("seed.testnet.miguelkoin.sprovoost.nl");
-        vSeeds.emplace_back("testnet-seed.bluematt.me"); // Just a static list of stable node(s), only supports x9
+        vSeeds.emplace_back("miguelkoin.duckdns.org"); // Just a static list of stable node(s), only supports x9
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
